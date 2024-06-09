d08a076a86f103c701cef958bc9f70ee64c2e91e
Akriti <upadhayaakriti23@gmail.com>
2024-06-09 20:30:11 +0545

    fixed bugs and made text display on the image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,34 +1,49 @@
 import { useState } from "react"
 import Data from "./Data"
 export default function Meme(){
-    const[memeImage, setMemeImage]= useState("https:\/\/i.imgflip.com\/1g8my4.jpg")
+    const[meme, setMeme]=useState({
+        topText:"",
+        bottomText:"",
+        randomImage: "https:\/\/i.imgflip.com\/1g8my4.jpg"
+
+    })
+    const[memeImage, setMemeImage]= useState(Data)
+    
+    function handleClick(){
+        const memesList = memeImage.data.memes
+        const randomNumber= Math.floor(Math.random()*memesList.length)
+        const url=memesList[randomNumber].url
+        setMeme(prevMeme=>({
+            ...prevMeme, randomImage: url
+        }))
+    }
+    function handleChange(event){
+        const {name,value}= event.target
+        setMeme(prevMeme=>({
+             ...prevMeme,
+             [name]:value
+        }))
+    }
     return(
         <>
         <div className="container">
         <form className="Form">
-            <div className="text-1">
-            <input type="text" value="Top text" className="text-1-discription"/>
-            </div>
-            <div className="text-2">
-                <input type="text" value="Buttom text" className="text-2-discription"/>
-            </div>
-            </form>
+          <input type="text" placeholder="Top Text" className="text-1-discription" name="topText" value={meme.topText} onChange={handleChange}/>
+            <input type="text" placeholder="Bottom text" className="text-2-discription" name="bottomText" value={meme.bottomText}   onChange={handleChange}/>
+          </form>
             <div className="btn">
             <button onClick={handleClick}  className="form-button">Get a new meme image</button>
-             
+             </div>
+              <div className="image">
+                <img src={meme.randomImage} alt="no img" className="image--1"/>
+            <div className="top-text">
+                <h2 className="image-top">{meme.topText}</h2>
+                </div>
+            <div className="bottom-text">
+            <h2 className="image-bottom">{meme.bottomText}</h2>
             </div>
-            <div className="image">
-            <img src={memeImage} alt="no img" />
             </div>
-           
-            </div>
-            
+           </div>
         </>
-       
     )
-    function handleClick(){
-        const memesList = Data.data.memes
-        const randomNumber= Math.floor(Math.random()*memesList.length)
-        setMemeImage(memesList[randomNumber].url)
-    }
-}
+}   
